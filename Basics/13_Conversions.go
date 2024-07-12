765c4cc1277a9308a5894cca627eab1a4d4920aa
LeoMangot5 <leokuo@mangot5.com>
2024-07-12 14:53:02 +0800

    Basics done

---
+++ Basics/13_Conversions.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	v1 := 42
+	fmt.Printf("v is of type %T\n", v1)
+	v2 := 3.14
+	fmt.Printf("v is of type %T\n", v2)
+	v3 := 0.867 + 0.5i
+	fmt.Printf("v is of type %T\n", v3)
+}

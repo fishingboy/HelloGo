be934deca45baba670a5ceab1abdcdfe2b71e877
Leo Kuo <et282523@hotmail.com>
2024-07-22 19:53:30 +0800

    float64 to int32

--- Test/3_Var2.go
+++ Test/3_Var2.go
@@ -2,17 +2,22 @@ package main
 
 import (
 	"fmt"
-	"strconv"
+	"math"
 )
 
 func main() {
 	a := float64(9999995)
-	fmt.Println(a)
-
-	before, err := strconv.Atoi(fmt.Sprint(a))
-	if err != nil {
-		fmt.Println("err!!")
+	var before int32
+	for i := 0; i < 10; i++ {
+		a++
+		fmt.Println(a)
+		before = int32(math.Round(a))
+		fmt.Printf("before = %v %T\n", before, before)
 	}
 
-	fmt.Printf("before = %v %T", before, before)
+	//before, err := strconv.Atoi(fmt.Sprint(a))
+	//if err != nil {
+	//	fmt.Println("err!!")
+	//}
+
 }
